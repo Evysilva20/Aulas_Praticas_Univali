2e6de50ffc8f335619c6dbcefaa7dae5aadb28a3
Evily Silva <evysilva784@gmail.com>
2026-04-24 11:03:59 -0300

    adicionando a lógica funcional com js

---
+++ Calculadora/javascript.js
@@ -0,0 +1,14 @@
+    const display = document.getElementById('display');
+
+        function adicionarAoVisor(valor) {
+            display.value += valor;}
+
+        function limparVisor() {
+            display.value = "";}
+
+        function calcular() {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = "Erro";
+                setTimeout(limparVisor, 1500);}}
